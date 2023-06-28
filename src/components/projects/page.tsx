ece0a6716427ecf986980418510c48b08b39a9ff
Stephanie Guzman <sguzman247@gmail.com>
2023-06-28 15:38:27 -0500

    feat: create projects component to display project cards

--- src/components/projects/page.tsx
+++ src/components/projects/page.tsx
@@ -1,7 +1,23 @@
+import { ProjectCard } from "../projectCard/page"
+import { PROFILE_PROJECTS_TITLE, PROFILE_PROJECTS_SUBTITLE, PROJECTS_DATA } from "@/lib/constants"
+
 export const Projects = () => {
   return (
     <section>
-      <p>Projects</p>
+      <h2>{PROFILE_PROJECTS_TITLE}</h2>
+      <h3>{PROFILE_PROJECTS_SUBTITLE}</h3>
+      <div>
+        {PROJECTS_DATA.map( (project, index) => {
+          return <ProjectCard 
+            id={project.id}
+            name={project.name}
+            headline={project.headline}
+            techStackLabels={project.techStackLabels}
+            image={project.images[0]}
+            key={index}
+          />
+        })}
+      </div>
     </section>
   )
 }
