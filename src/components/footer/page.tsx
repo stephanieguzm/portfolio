c0feab35a2c6079fbab5eb8803445110307acaf7
Stephanie Guzman <sguzman247@gmail.com>
2023-06-28 12:30:06 -0500

    feature: create initial footer

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,8 +1,19 @@
+import { SITE_NAME , PROFILE_GITHUB, PROFILE_LINKEDIN} from "@/lib/constants"
+
+const DATE = new Date()
+const YEAR = DATE.getUTCFullYear()
+
 const Footer = () => {
   return (
-    <section>
-      <p>Footer</p>
-    </section>
+    <footer>
+      <div>
+        <p>&#169; {YEAR} {SITE_NAME}. All rights reserved.</p>
+        <div>
+          <a href={PROFILE_GITHUB} target='_blank' rel='noreferrer'>GitHub</a>
+          <a href={PROFILE_LINKEDIN} target='_blank' rel='noreferrer'>LinkedIn</a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
