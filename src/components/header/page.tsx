d747950ab16e645784724351be8967462b06802f
Stephanie Guzman <sguzman247@gmail.com>
2023-06-28 12:54:04 -0500

    chore: add header name and navigation links

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,8 +1,30 @@
+"use client"
+
+import { SITE_NAME } from "@/lib/constants"
+import Link from "next/link"
+
+interface NavLink {
+  path: string
+  name: string
+}
+
+const navLinks : NavLink[] = [
+  { path: "about", name: "About" },
+  { path: "skills", name: "Skills" },
+  { path: "projects", name: "Projects" },
+  { path: "connect", name: "Connect"}
+]
+
 const Header = () => {
   return (
-    <section>
-      <p>Header</p>
-    </section>
+    <header>
+      <p>{SITE_NAME}</p>
+      <div>
+        {navLinks.map( (link, index) => 
+          <Link key={index} href={`/${link.path}`}>{link.name}</Link>
+        )}
+      </div>
+    </header>
   )
 }
 
