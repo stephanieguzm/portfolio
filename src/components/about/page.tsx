7be22a38d1a0de583fb5b4660c145ee813b588ec
Stephanie Guzman <sguzman247@gmail.com>
2023-06-28 14:26:01 -0500

    feat: add component copy

--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -1,7 +1,10 @@
+import { PROFILE_ABOUT_TITLE, PROFILE_ABOUT_TEXT } from "@/lib/constants"
+
 export const About = () => {
   return (
-    <section>
-      <p>About</p>
+    <section className={"about"}>
+      <h2>{PROFILE_ABOUT_TITLE}</h2>
+      <p>{PROFILE_ABOUT_TEXT}</p>
     </section>
   )
 }
