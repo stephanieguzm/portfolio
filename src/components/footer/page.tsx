ba90fea658d6619a712872196bb014374d95a584
Stephanie Guzman <sguzman247@gmail.com>
2023-06-28 12:45:42 -0500

    chore: add data types to footer

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,13 +1,13 @@
-import { SITE_NAME , PROFILE_GITHUB, PROFILE_LINKEDIN} from "@/lib/constants"
+import { COPYRIGHT , PROFILE_GITHUB, PROFILE_LINKEDIN} from "@/lib/constants"
 
-const DATE = new Date()
-const YEAR = DATE.getUTCFullYear()
+const DATE : Date = new Date()
+const YEAR : number = DATE.getUTCFullYear()
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <p>&#169; {YEAR} {SITE_NAME}. All rights reserved.</p>
+        <p>&#169; {YEAR} {COPYRIGHT}. All rights reserved.</p>
         <div>
           <a href={PROFILE_GITHUB} target='_blank' rel='noreferrer'>GitHub</a>
           <a href={PROFILE_LINKEDIN} target='_blank' rel='noreferrer'>LinkedIn</a>
