83a332d8f7963a2e8782f05806206f7fc3da5963
Stephanie Guzman <sguzman247@gmail.com>
2023-06-30 11:39:25 -0500

    feat: add external href links to project page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -29,17 +29,20 @@ export const generateStaticParams = async () => {
 const ProjectPage = async ({ params }: { params: { id: string } }) => {
   const result = await getSingleProject({ params })
   const project = result
-  console.log('project', project)
 
   return (
     <section id={project.id}>
       <h2>{project.name}</h2>
       <h3>{project.headline}</h3>
-        <div>
-      {/* {repoURL.length == 1 ? <a href={repoURL[0]} target='_blank' rel='noreferrer'>View Code</a> :
-          <a href={repoURL[1]} target='_blank' rel='noreferrer'>UI Repo</a>} */}
-      <a href={project.liveURL} target='_blank' rel='noreferrer'>Live Site</a>
-        </div>
+      <div>
+        {project.repoURL.length == 1 ? <a href={project.repoURL[0]} target='_blank' rel='noreferrer'>View Code</a> :
+          <>
+            <a href={project.repoURL[0]} target='_blank' rel='noreferrer'>UI Code</a>
+            <a href={project.repoURL[1]} target='_blank' rel='noreferrer'>API Code</a>
+          </>
+        }
+          <a href={project.liveURL} target='_blank' rel='noreferrer'>Live Site</a>
+      </div>
     </section>
   )
 }
