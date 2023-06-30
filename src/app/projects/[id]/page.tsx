d63d2a90ea85e29785425b79af83948f3cd0213a
Stephanie Guzman <sguzman247@gmail.com>
2023-06-30 10:07:32 -0500

    feat: add dynamic routing to display project page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,11 +1,47 @@
-interface ProjectProps {}
+import { getAllProjects, getSingleProject } from "@/lib/projectdata"
 
-const Project = ( props : ProjectProps ) => {
+export const dynamicParams = false
+export interface ProjectPageProps {
+  id: string
+}
+export interface ProjectInterface {
+    id: string
+    name: string
+    headline: string
+    images: string[]
+    techStackLabels: string[]
+    repoURL: string[]
+    liveURL: string
+    purpose?: string
+    techStackExp?: string
+    problems?: string
+    lessons?: string
+}
+
+export const generateStaticParams = async () => {
+  const projects : ProjectInterface[] = await getAllProjects()
+
+  return projects.map(project => {
+    return { id : project.id }
+  })
+}
+
+const ProjectPage = async ({ params }: { params: { id: string } }) => {
+  const result = await getSingleProject({ params })
+  const project = result
+  console.log('project', project)
+
   return (
-    <div>
-      <p>Single Project</p>
-    </div>
+    <section id={project.id}>
+      <h2>{project.name}</h2>
+      <h3>{project.headline}</h3>
+        <div>
+      {/* {repoURL.length == 1 ? <a href={repoURL[0]} target='_blank' rel='noreferrer'>View Code</a> :
+          <a href={repoURL[1]} target='_blank' rel='noreferrer'>UI Repo</a>} */}
+      <a href={project.liveURL} target='_blank' rel='noreferrer'>Live Site</a>
+        </div>
+    </section>
   )
 }
 
-export default Project
+export default ProjectPage
