5448d0a2dbdd011bd4e0db886e486b61f15e9982
Stephanie Guzman <sguzman247@gmail.com>
2023-07-01 10:07:58 -0500

    fix: update link path to route to baseUrl/path

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,27 +1,38 @@
 "use client"
 
-import { SITE_NAME } from "@/lib/constants"
+import { SITE_NAME } from "@lib/constants"
 import Link from "next/link"
-
+import { usePathname } from "next/navigation" 
 interface NavLink {
   path: string
+  id: string
   name: string
 }
 
 const navLinks : NavLink[] = [
-  { path: "about", name: "About" },
-  { path: "skills", name: "Skills" },
-  { path: "projects", name: "Projects" },
-  { path: "connect", name: "Connect"}
+  { path: "#about", id: "about", name: "About" },
+  { path: "#skills", id: "skills", name: "Skills" },
+  { path: "#projects", id: "projects", name: "Projects" },
+  { path: "#connect", id: "connect", name: "Connect"}
 ]
 
 const Header = () => {
+const pathname = usePathname()
+
   return (
-    <header>
+    <header id={"header"} >
       <p>{SITE_NAME}</p>
       <div>
-        {navLinks.map( (link, index) => 
-          <Link key={index} className={`nav-link`} href={`#${link.path}`}>{link.name}</Link>
+        {navLinks.map((link, index) => 
+          {
+            const isActive = pathname.startsWith(link.id)
+            return (<Link 
+              key={index}
+              id={link.id}
+              href={`/${link.path}`}>
+                {link.name}
+            </Link>)
+          }
         )}
       </div>
     </header>
