c037ed32bb2b70b462da4dd8d3463f41deeeb060
Stephanie Guzman <sguzman247@gmail.com>
2023-07-02 09:57:08 -0500

    chore: update nodemailer smtp options

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -2,10 +2,11 @@ import nodemailer from "nodemailer"
 
 export const EMAIL_USER = process.env.NEXT_PUBLIC_EMAIL_USER;
 export const EMAIL_PASS = process.env.NEXT_PUBLIC_EMAIL_PASS; 
-export const EMAIL_SERVICE = process.env.NEXT_PUBLIC_EMAIL_HOST
+export const EMAIL_HOST = process.env.NEXT_PUBLIC_EMAIL_HOST
 
 export const transporter = nodemailer.createTransport({
-  service: EMAIL_SERVICE,
+  host: EMAIL_HOST,
+  port: 587,
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS,
