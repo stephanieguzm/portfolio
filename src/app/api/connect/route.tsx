93211a12f8495fc944d70121e5e1627c942192c4
Stephanie Guzman <sguzman247@gmail.com>
2023-07-05 13:07:14 -0500

    fix: remove .then() from try statement to resolve 500 server error + remove console logs

--- src/app/api/connect/route.tsx
+++ src/app/api/connect/route.tsx
@@ -3,9 +3,7 @@ import nodemailer from "nodemailer"
 import { EMAIL_USER, transporter } from "@/lib/nodemailer";
 
 export async function POST(req: Request) {
-  const body = await req.json();
-
-  console.log(body)
+  const body = await req.json()
 
   const { name, email, message } : {
     name: string
@@ -13,33 +11,22 @@ export async function POST(req: Request) {
     message: string
   } = body
 
-  console.log("name", name, "email", email, "message", message)
-
   const mailOptions : nodemailer.SendMailOptions = {
     from: email,
     to: EMAIL_USER,
     subject: `New Portfolio Contact Form Submission`,
     text: `${name} has sent you the following message : ${message}`,
     html: `<div>${message} <br /><br />Thank you,<br />${name}</div>`
-  };
+  }
 
   try {
-    await transporter
-    .sendMail(mailOptions)
-    .then((response: nodemailer.SentMessageInfo) => {
-      if(response.OK) {
-        return NextResponse.json(
-          { error: false, emailSent: true, errors: [], response },
-          { status: 200 }
-        )
-
-      }
-    })
-  } 
-  catch (error: nodemailer.SentMessageInfo) {
+    await transporter.sendMail(mailOptions)
     return NextResponse.json(
-      { error: true, emailSent: false, errors: [error] },
-      { status: 500 }
-    );
+      { error: false, emailSent: true, errors: [], status: 200 },
+    )
+  } catch (error: nodemailer.SentMessageInfo) {
+    return NextResponse.json(
+      { error: true, emailSent: false, errors: [error], status: 500 },
+    )
   } 
 }
