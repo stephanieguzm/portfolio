fb719255ef85de14377073f43f6544424b39475d
Stephanie Guzman <sguzman247@gmail.com>
2023-07-06 15:09:30 -0500

    chore: add styling to about component

--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -2,9 +2,9 @@ import { PROFILE_ABOUT_TITLE, PROFILE_ABOUT_TEXT } from "@lib/constants"
 
 export const About = () => {
   return (
-    <section id={"about"}>
-      <h2>{PROFILE_ABOUT_TITLE}</h2>
-      <p>{PROFILE_ABOUT_TEXT}</p>
+    <section id={"about"} className={"m-4"}>
+      <h2 className={"text-3xl font-semibold leading-7 text-gray-900 py-1"}>{PROFILE_ABOUT_TITLE}</h2>
+      <p className={"mt-1 text-md leading-6 text-gray-600 py-4"}>{PROFILE_ABOUT_TEXT}</p>
     </section>
   )
 }
