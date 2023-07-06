7163da07cf28915b316025dbad5e69a9f6ba1fe5
Stephanie Guzman <sguzman247@gmail.com>
2023-07-06 12:21:27 -0500

    feat: add styling to form component

--- src/components/form/page.tsx
+++ src/components/form/page.tsx
@@ -63,30 +63,32 @@ const Form = () => {
   }
 
   return(
-    <form onSubmit={handleSubmit}>
-      <div className={"form-inner"}>
-        <label htmlFor={"name"}>
-          Name
-        </label>
-        <input 
-          id={"name"}
-          type={"text"} 
-          name={"name"} 
-          className={""}
-          value={formData.name}
-          onChange={handleChange}
-          minLength={2}
-          maxLength={40}
-          required />
-        <div>
-          <label htmlFor={"email"} className={""}>
+    <form onSubmit={handleSubmit} className={"w-full max-w-lg"}>
+      <div className={"flex flex-wrap -mx-3 mb-6"}>
+        <div className={"w-full md:w-1/2 px-3 mb-6 md:mb-0"}>
+          <label htmlFor={"name"} className={"block tracking-wide mb-2 text-sm font-medium leading-6 text-gray-900"}>
+            Name
+          </label>
+          <input 
+            id={"name"}
+            type={"text"} 
+            name={"name"} 
+            className={"appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded-md shadow-sm py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 ring-0.5 ring-inset ring-gray-300 focus-within:ring-1 focus-within:ring-inset focus-within:ring-purple-400"}
+            value={formData.name}
+            onChange={handleChange}
+            minLength={2}
+            maxLength={40}
+            required />
+        </div>
+        <div className={"w-full md:w-1/2 px-3"}>
+          <label htmlFor={"email"} className={"block tracking-wide mb-2 text-sm font-medium leading-6 text-gray-900"}>
             Email
           </label>
           <input 
             id={"email"}
             type={"email"} 
             name={"email"} 
-            className={""}
+            className={"appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded-md shadow-sm py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 ring-0.5 ring-inset ring-gray-300 focus-within:ring-1 focus-within:ring-inset focus-within:ring-purple-400"}
             value={formData.email}
             onChange={handleChange}
             minLength={3}
@@ -94,43 +96,43 @@ const Form = () => {
             pattern={emailRegex}
             required />
         </div>
-        <div>
-          <label htmlFor={"message"} className={""}>
-            Message
-          </label>
-          <textarea 
-            id={"message"}
-            name={"message"}
-            className={""}
-            value={formData.message}
-            onChange={handleChange}
-            minLength={5}
-            maxLength={750}
-            required />
-        </div>
-        <button
-          type={"submit"}
-          className={"btn btn-primary btn-send-message"}
-          disabled={status === formStatus.loading} >
-          {status === formStatus.loading ? (
-            <>
-              <i className={"fa-solid fa-spinner fa-spin"}></i>Sending...
-            </>
-          ) : (
-            <>Send Message</>
-            )}
-        </button>
-        {status === formStatus.error && (
-          <div className="alert alert-danger">
-            There was an error sending your message. Please try again.
-          </div>
-        )}
-        {status === formStatus.submitted && (
-          <div className="alert alert-success contact_msg" role="alert">
-            Your message was sent successfully.
-          </div>
-        )}
       </div>
+      <div>
+        <label htmlFor={"message"} className={"block tracking-wide mb-2 text-sm font-medium leading-6 text-gray-900"}>
+          Message
+        </label>
+        <textarea 
+          id={"message"}
+          name={"message"}
+          className={"appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded-md shadow-sm py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 ring-0.5 ring-inset ring-gray-300 focus-within:ring-1 focus-within:ring-inset focus-within:ring-purple-400"}
+          value={formData.message}
+          onChange={handleChange}
+          minLength={5}
+          maxLength={750}
+          required />
+      </div>
+      <button
+        type={"submit"}
+        className={"text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 my-4"}
+        disabled={status === formStatus.loading} >
+        {status === formStatus.loading ? (
+          <>
+            <i className={"fa-solid fa-spinner fa-spin"}></i>Sending...
+          </>
+        ) : (
+          <>Send Message</>
+          )}
+      </button>
+      {status === formStatus.error && (
+        <div className={"text-red-700 font-medium py-2.5 leading-6"}>
+          There was an error sending your message. Please try again.
+        </div>
+      )}
+      {status === formStatus.submitted && (
+        <div className={"text-purple-700 font-medium py-2.5 leading-6"}>
+          Your message was sent successfully.
+        </div>
+      )}
     </form>
   )
 }
