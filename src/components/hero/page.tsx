18a8c5144ce06b6f4665c3603ffebf7f618b48c6
Stephanie Guzman <sguzman247@gmail.com>
2023-07-07 12:52:00 -0500

    feat: add typing animation to hero

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -1,24 +1,36 @@
 "use client"
 
-import { HERO_TITLE, HERO_SUBTITLE, HERO_SUBTITLE_LABELS } from "@/lib/constants"
+import { SITE_NAME, HERO_SUBTITLE, HERO_SUBTITLE_LABELS } from "@/lib/constants"
 import { useEffect, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 
 export const Hero = () => {
 
   let reducedMotion : MediaQueryList
-  const [ reduced, setReduced ] = useState(false)
+  const [ reduced, setReduced ] = useState(true)
 
   useEffect(() => {
    reducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)")
    setReduced(reducedMotion.matches)
   }, [])
 
+  const labels : (string | number)[] = HERO_SUBTITLE_LABELS.map(label => [label, 1000]).flat()
+
   return (
-    <section id={"hero"} className={"mx-auto flex h-[88vh] w-full items-center justify-center bg-fuchsia-400 px-4 text-center text-gray-900 md:px-8"}>
+    <section id={"hero"} className={"mx-auto flex h-[88vh] w-full items-center justify-center bg-fuchsia-200 px-4 text-center text-gray-900 md:px-8"}>
       <div>
-        <h1>{HERO_TITLE}</h1>
-        {!reduced && <h2>{HERO_SUBTITLE_LABELS}</h2>}
-        {reduced && <h2>{HERO_SUBTITLE}</h2>}
+        <h1 className={"text-4xl"}>Hi there, I'm 
+          <span className={"text-fuchsia-950"}> {SITE_NAME}</span>
+        </h1>
+        {reduced && <h2 className="text-3xl">{HERO_SUBTITLE}</h2>}
+        {!reduced && <TypeAnimation
+          preRenderFirstString={true}
+          sequence={labels}
+          wrapper="span"
+          cursor={true}
+          repeat={Infinity}
+          style={{ fontSize: "1.875em", lineHeight: "2.25em", display: "inline-block" }}
+        />}
       </div>
     </section>
   )
