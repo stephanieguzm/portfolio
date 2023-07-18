d3b82fb0a0e263af4f2b32f0a2a499eb8bfe6468
Stephanie Guzman <sguzman247@gmail.com>
2023-07-18 16:50:00 -0500

    chore: update links and link styling

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -5,39 +5,41 @@ import Link from "next/link"
 import { usePathname } from "next/navigation" 
 interface NavLink {
   path: string
-  id: string
   name: string
 }
 
 const navLinks : NavLink[] = [
-  { path: "#about", id: "about", name: "About" },
-  { path: "#skills", id: "skills", name: "Skills" },
-  { path: "#projects", id: "projects", name: "Projects" },
-  { path: "#connect", id: "connect", name: "Connect"}
+  { path: "about", name: "About" },
+  { path: "#skills", name: "Skills" },
+  { path: "projects", name: "Projects" },
+  { path: "connect", name: "Connect"}
 ]
 
 const Header = () => {
-const pathname = usePathname()
+  const pathname = usePathname()
 
   return (
     <header id={"header"} className={"sticky top-0 z-50 mx-auto flex h-[12vh] w-full items-center justify-center bg-white px-4 text-center text-gray-900 md:px-8"}>
       <div className={"flex w-full max-w-3xl flex-col items-center justify-center space-y-4 md:max-w-7xl md:flex-row md:justify-between md:space-y-0"}>
         <div><Link href={`/`}className={"mb-2 font-medium leading-6 text-gray-900"}>{SITE_NAME}</Link></div>
-        <div className={"flex flex-row space-x-4"}>
-          {navLinks.map((link, index) => {
+        <nav>
+          <div className={"flex flex-row space-x-4"}>
+            {navLinks.map((link, index) => {
               const isActive = pathname.startsWith(`/${link.path}`)
+              const activeStyle = `text-purple-700 border-b-2 border-purple-700`
+
               return (<Link 
                 key={index}
-                id={link.id}
+                id={link.path}
                 href={`/${link.path}`}
-                className={`px-3 py-2 text-gray-900 font-medium hover:text-purple-700 ${isActive ? "border-b-2 border-blue-600" : ""}`}
-                passHref
-                >
+                className={`px-3 py-2 text-gray-900 font-medium hover:text-purple-700 transition-transform duration-500 ease-in-out md:hover:-translate-y-1 ${isActive && activeStyle}`}
+                scroll={false}
+                passHref>
                   {link.name}
               </Link>)
-            }
-          )}
-        </div>
+            })}
+          </div>
+        </nav>
       </div>
     </header>
   )
