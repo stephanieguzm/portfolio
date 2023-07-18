a164e024ad6bda4d472fe2f31a67334f8b8496c1
Stephanie Guzman <sguzman247@gmail.com>
2023-07-18 16:05:40 -0500

    chore: add styling to projects component

--- src/components/projects/page.tsx
+++ src/components/projects/page.tsx
@@ -6,10 +6,10 @@ export const Projects = async () => {
   const projectsData = await getAllProjects()
 
   return (
-    <section id={"projects"} className={"md:px-8 m-4"} >
+    <section id={"projects"} className={"flex flex-col items-center justify-center text-gray-900 md:px-8 m-4 my-10"} >
       <h2 className={"text-3xl font-semibold leading-7 text-gray-900 py-1"}>{PROFILE_PROJECTS_TITLE}</h2>
       <h3 className={"mt-1 text-md leading-6 text-gray-600 py-4"}>{PROFILE_PROJECTS_SUBTITLE}</h3>
-      <div>
+      <div className={"flex flex-row justify-center"}>
         {projectsData.map( (project, index) => {
           return <ProjectCard 
             id={project.id}
