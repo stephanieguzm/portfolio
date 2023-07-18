fde10e6c29f2c56ceddc297d6933d519b91891d6
Stephanie Guzman <sguzman247@gmail.com>
2023-07-18 17:49:30 -0500

    chore: remove components from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 import Image from 'next/image'
 import { Hero } from '@components/hero/page'
-import { About } from '@components/about/page'
-import { Skills } from '@components/skills/page'
-import { Projects } from '@components/projects/page'
-import { Connect } from '@components/connect/page'
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Connect />
     </main>
   )
 }
