cf110356e3db5b0484d91f06ec56ad3269f44a3e
Stephanie Guzman <sguzman247@gmail.com>
2023-07-19 16:29:13 -0500

    feat: add About Me title to page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,8 @@ import { Skills } from "@components/skills/page"
 
 const About = () => {
   return (
-    <section id={"about"} className={"flex flex-col items-center justify-center text-gray-900 md:px-8 m-4 my-10"}>
+    <section id={"about"} className={"flex flex-col items-start justify-start text-gray-900 md:px-8 m-4"}>
+      <h1 className={"text-4xl font-bold pb-8"}>About Me</h1>
       <div className={"flex-col w-full max-w-2xl items-center justify-center space-y-4 md:max-w-7xl md:justify-between md:space-y-0"}>
         <h2 className={"text-3xl font-semibold leading-7 text-gray-900 py-1" }>{PROFILE_ABOUT_TITLE}</h2>
         {PROFILE_ABOUT_TEXT.map((para, index) => {
