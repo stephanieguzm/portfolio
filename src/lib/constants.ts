935fa7138dcbfca1d29e4838aa18d05076ab7a05
Stephanie Guzman <sguzman247@gmail.com>
2023-07-20 23:24:08 -0500

    chore: update constants info

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,7 +10,7 @@ import {
   SiCypress as Cypress,
   SiGit as Git,
   SiGithub as GitHub
-} from "react-icons/si"
+} from "react-icons/si/index"
 
 export const icons = {
   React, HTML, CSS, JavaScript, TypeScript, Express, Mocha, Chai, Cypress, Git, GitHub
@@ -25,14 +25,16 @@ export const SITE_NAME : string = `Stephanie`
 export const SITE_DESCRIPTION : string = `Portfolio Website`
 export const COPYRIGHT : string = `Stephanie Guzm\u00E1n`
 
+export const ABOUT_IMG_ALT : string =`Stephanie Guzm\u00E1n wearing a black top standing against a mint green background.`
+
 export const HERO_TITLE : string = `Hi there, I'm Stephanie Guzm\u00E1n`
 export const HERO_SUBTITLE : string = `Front End Software Developer`
 export const HERO_SUBTITLE_LABELS : string[] = [ "Web Developer", "React Developer", "UI Developer", "Front End Developer" ]
 
-export const PROFILE_ABOUT_TITLE : string = `I build products with impact`
-export const PROFILE_ABOUT_TEXT : string[] = [`Curious and altruistic by nature, I spent a decade in social work helping people make meaningful changes in their lives. I collaborated with people, troubleshot with them through challenges, and equipped them with the tools and perspective to solve their problems. I thrived in spaces that channelled my creativity, problem-solving, and systems thinking.`,
+export const PROFILE_ABOUT_TITLE : string = `I'm building products with impact`
+export const PROFILE_ABOUT_TEXT : string[] = [`I'm a collaborative problem-solver driven by a desire to help people thrive in their lives. Originally, this led me to pursue a career in social work where I helped families find safety and stability after homelessness. I collaborated with people, problem-solving and connecting them with the tools and resources to work through each challenge. I thrived in spaces that channelled my creativity, problem-solving, and systems thinking.`,
 `I reached a point when I was ready to grow in a new career and solve problems with greater impact, and software development quickly became my outlet. Seeing my first web page come to life on my screen opened up a world of possibilities. I had discovered a new way to make an impact and I haven’t looked back.`,
-`Helping people learn, grow, and connect with one another is at the heart of what I do. Fueled by my curiosity and eagerness to solve problems collaboratively, I'm eager to contribute my technical skills to an organization that's building digital products that positively impact the way people live.`]
+`Now as a developer, my goal is to build tools and products that improve people's quality of life. Fueled by my curiosity and eagerness to solve problems that create social good, I'm eager to contribute my technical skills to an organization that's building digital products that positively impact the way people live.`]
 
 export const PROFILE_SKILLS_TITLE : string = `My Tech Stack`
 export const PROFILE_SKILLS_LABELS : SkillsLabel[] = [
