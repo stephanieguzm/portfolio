bad0bff959f3603a14d36afc375aa2b2be190e9e
Stephanie Guzman <sguzman247@gmail.com>
2023-07-21 00:08:32 -0500

    chore: change icon img srcs, update about tcopy

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,24 +1,6 @@
-import { 
-  SiReact as React, 
-  SiHtml5 as HTML, 
-  SiCss3 as CSS, 
-  SiJavascript as JavaScript, 
-  SiTypescript as TypeScript,  
-  SiExpress as Express,
-  SiMocha as Mocha,
-  SiChai as Chai,
-  SiCypress as Cypress,
-  SiGit as Git,
-  SiGithub as GitHub
-} from "react-icons/si/index"
-
-export const icons = {
-  React, HTML, CSS, JavaScript, TypeScript, Express, Mocha, Chai, Cypress, Git, GitHub
-}
-
 export interface SkillsLabel {
   skill: string
-  icon: keyof typeof icons
+  src: string
 }
 
 export const SITE_NAME : string = `Stephanie`
@@ -33,54 +15,54 @@ export const HERO_SUBTITLE_LABELS : string[] = [ "Web Developer", "React Develop
 
 export const PROFILE_ABOUT_TITLE : string = `I'm building products with impact`
 export const PROFILE_ABOUT_TEXT : string[] = [`I'm a collaborative problem-solver driven by a desire to help people thrive in their lives. Originally, this led me to pursue a career in social work where I helped families find safety and stability after homelessness. I collaborated with people, problem-solving and connecting them with the tools and resources to work through each challenge. I thrived in spaces that channelled my creativity, problem-solving, and systems thinking.`,
-`I reached a point when I was ready to grow in a new career and solve problems with greater impact, and software development quickly became my outlet. Seeing my first web page come to life on my screen opened up a world of possibilities. I had discovered a new way to make an impact and I haven’t looked back.`,
-`Now as a developer, my goal is to build tools and products that improve people's quality of life. Fueled by my curiosity and eagerness to solve problems that create social good, I'm eager to contribute my technical skills to an organization that's building digital products that positively impact the way people live.`]
+`I reached a point when I was ready to grow in a new career and solve issues with greater impact, and software development quickly became my outlet. Seeing my first web page come to life on my screen opened up a world of possibilities. I had discovered a new way to make an impact and I haven’t looked back.`,
+`Now as a developer, my goal is to build tools and products that improve people's quality of life. Fueled by my curiosity and eagerness to solve problems that create social good, I'm excited to contribute my technical skills to an organization that's building digital products that positively impact the way people live.`]
 
 export const PROFILE_SKILLS_TITLE : string = `My Tech Stack`
 export const PROFILE_SKILLS_LABELS : SkillsLabel[] = [
   { 
     skill: "React",
-    icon: "React"
+    src: "/images/site/react.svg"
   },
   {
     skill: "HTML",
-    icon: "HTML"
+    src: "/images/site/html5.svg"
   },
   {
     skill: "CSS",
-    icon: "CSS"
+    src: "/images/site/css3.svg"
   },
   {
     skill: "JavaScript",
-    icon: "JavaScript"
+    src: "/images/site/javascript.svg"
   },
   {
     skill: "TypeScript",
-    icon: "TypeScript"
+    src: "/images/site/typescript.svg"
   },
   {
     skill: "Express",
-    icon: "Express"
+    src: "/images/site/express.svg"
   },
   {
     skill: "Mocha",
-    icon: "Mocha"
+    src: "/images/site/mocha.svg"
   },
   {
     skill: "Chai",
-    icon: "Chai"
+    src: "/images/site/chai.svg"
   },
   {
     skill: "Cypress",
-    icon: "Cypress"
+    src: "/images/site/cypress.svg"
   },
   {
     skill: "Git",
-    icon: "Git"
+    src: "/images/site/git.svg"
   },
   {
     skill: "GitHub",
-    icon: "GitHub"
+    src: "/images/site/github-s.svg"
   },
 ]
 
