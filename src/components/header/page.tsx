f38b0956926d440c9e798b4168a979ac6bf1520b
Stephanie Guzman <sguzman247@gmail.com>
2023-07-20 23:29:34 -0500

    chore: add header border

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -3,6 +3,7 @@
 import { SITE_NAME } from "@lib/constants"
 import Link from "next/link"
 import { usePathname } from "next/navigation" 
+
 interface NavLink {
   path: string
   name: string
@@ -18,7 +19,7 @@ const Header = () => {
   const pathname = usePathname()
 
   return (
-    <header id={"header"} className={"sticky top-0 z-50 mx-auto flex h-[12vh] w-full items-center justify-center bg-white px-4 text-center text-gray-900 md:px-8"}>
+    <header id={"header"} className={"sticky top-0 z-50 mx-auto flex h-[8vh] w-full items-center justify-center bg-white px-4 text-center text-gray-900 border-b border-gray-100 md:px-8"}>
       <div className={"flex w-full max-w-3xl flex-col items-center justify-center space-y-4 md:max-w-7xl md:flex-row md:justify-between md:space-y-0"}>
         <div><Link href={`/`}className={"mb-2 font-medium leading-6 text-gray-900"}>{SITE_NAME}</Link></div>
         <nav>
