e9990fa4ee2c099f3e8cb1de063ff369186c2734
Stephanie Guzman <sguzman247@gmail.com>
2023-07-20 23:23:32 -0500

    feat: remove react-icons and add svgs

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,28 +1,34 @@
-import { LuGithub, LuLinkedin } from "react-icons/lu"
 import { COPYRIGHT , PROFILE_GITHUB, PROFILE_LINKEDIN} from "@lib/constants"
+import Image from "next/image"
 
 const DATE : Date = new Date()
 const YEAR : number = DATE.getUTCFullYear()
 
 const Footer = () => {
   return (
-    <footer id={"footer"} className={"mx-auto flex h-[12vh] w-full items-center justify-center bg-purple-100 px-4 text-center text-gray-900 md:px-8"}>
+    <footer id={"footer"} className={"relative mx-auto flex h-[8vh] w-full items-center justify-center bg-purple-100 px-4 text-center text-gray-900 md:px-8 border-t border-gray-100"}>
       <div className={"flex w-full max-w-3xl flex-col items-center justify-center space-y-4 md:max-w-7xl md:flex-row md:justify-between md:space-y-0"}>
         <div>
           <p className={"mb-2 text-sm font-medium leading-6 text-gray-900"}>&#169; {YEAR} {COPYRIGHT}. All rights reserved.</p>
         </div>
-        <div className={"flex flex-row space-x-4"}>
+        <div className={"flex flex-row space-x-4 text-gray-900"}>
           <a href={PROFILE_GITHUB} target='_blank' rel='noreferrer'>
-            <LuGithub
-              size={25}
+            <Image 
+              src={"/images/site/github.svg"}
+              alt={"GitHub icon"}
+              width={24}
+              height={24}
               className={"transition-transform duration-500 ease-in-out md:hover:-translate-y-1"}
             />
           </a>
           <a href={PROFILE_LINKEDIN} target='_blank' rel='noreferrer'>
-            <LuLinkedin 
-              size={25}
+              <Image 
+              src={"/images/site/linkedin.svg"}
+              alt={"LinkedIn icon"}
+              width={24}
+              height={24}
               className={"transition-transform duration-500 ease-in-out md:hover:-translate-y-1"}
-              />
+            />
           </a>
         </div>
       </div>
