1e55a808843342f10f3e48004ee0d5abb97e0e71
Stephanie Guzman <sguzman247@gmail.com>
2023-07-24 16:19:54 -0500

    feat: create reusable label component

---
+++ src/components/label/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+type LabelProps = {
+  skill: string
+  src: string
+}
+
+export const Label = ( { skill, src } : LabelProps) => {
+  return (
+    <>
+      <p className={"text-gray-900 font-medium rounded-full text-sm px-5 py-2.5 text-center mx-2 mb-2 my-4"}>{skill}</p>
+      <Image 
+        src={src}
+        alt={`${skill} icon`}
+        width={48}
+        height={48}
+        className={"hover:scale-125 transition duration-500"}
+      />
+    </>
+  )
+}
