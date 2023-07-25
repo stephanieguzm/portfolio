a32340bfea98a00b558cac4344a92a2f518b4603
Stephanie Guzman <sguzman247@gmail.com>
2023-07-25 14:08:17 -0500

    fix: unify layout styling with other pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,15 @@
 import { ProjectCard } from "../../components/projectCard/page"
-import { PROFILE_PROJECTS_TITLE, PROFILE_PROJECTS_SUBTITLE } from "@lib/constants"
+import { PROFILE_PROJECTS_SUBTITLE } from "@lib/constants"
 import { getAllProjects } from "@lib/projectdata"
 
 const Projects = async () => {
   const projectsData = await getAllProjects()
 
   return (
-    <section id={"projects"} className={"flex flex-col items-start justify-center text-gray-900 md:px-8 m-4 my-10"} >
-      <h2 className={"text-3xl font-semibold leading-7 text-gray-900 py-1"}>{PROFILE_PROJECTS_TITLE}</h2>
-      <h3 className={"mt-1 text-md leading-6 text-gray-600 py-4"}>{PROFILE_PROJECTS_SUBTITLE}</h3>
-      <div className={"flex flex-col justify-center"}>
+    <section id={"projects"} className={"flex flex-col items-center justify-center text-gray-900 md:px-8 m-8"} >
+      <h2 className={"text-4xl w-full max-w-2xl font-bold pb-4 text-left md:max-w-7xl"}>Projects</h2>
+      <h3 className={"mt-1 text-md leading-6 text-gray-600 py-2 md:py-4 w-full max-w-2xl text-left md:max-w-7xl"}>{PROFILE_PROJECTS_SUBTITLE}</h3>
+      <div className={"flex flex-col w-full max-w-2xl items-center justify-center space-y-4 md:max-w-7xl md:justify-between lg:flex-row"}>
         {projectsData.map( (project, index) => {
           return <ProjectCard 
             id={project.id}
